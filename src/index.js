fb999325503dee3f65e844d19a7925d20846556d
MariaNagornaya1980 <mariawright205@gmail.com>
2024-07-12 12:11:04 -0700

    updated search functionality with the use of Api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
 function searchCity(city) {
   let apiKey = "f3tb6c4a39o18b701a82bf50b801c4af";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 let searchFormElement = document.querySelector("#search-form");
 function handleSearchSubmit(event) {
@@ -14,3 +22,5 @@ function handleSearchSubmit(event) {
 
 let searchForm = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Scottsdale");
