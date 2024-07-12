fc88948cfece4b8e2e048342810f23648220c1b3
MariaNagornaya1980 <mariawright205@gmail.com>
2024-07-12 11:32:39 -0700

    added apiKey and apiUrl

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+function searchCity(city) {
+  let apiKey = "f3tb6c4a39o18b701a82bf50b801c4af";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+}
 let searchFormElement = document.querySelector("#search-form");
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
